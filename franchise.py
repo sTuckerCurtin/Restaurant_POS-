48d7fc55900f024d25a3e88f102ad3df64d35069
Samuel Curtin <stuckercurtin@gmail.com>
2023-04-13 14:18:44 -0400

    MVP FIXED

--- franchise.py
+++ franchise.py
@@ -9,18 +9,23 @@ price = float
 class Franchise:
     def __init__(self, location_number):
         self.location_number = location_number
-
-    def place_order(self):
-        order_type = input(f"From store {self.location_number } What would you like to order? Pizza, Pasta or a Salad? ")
-        if order_type == "Pizza":
-            order_price = 25
-        elif order_type == "Pasta":
-            order_price = 10.00
-        elif order_type == "Salad":
-            order_price = 5.00
         
+    def place_order(self):
+        while True:
+            order_type = input(f"Welcome to Fat Tucks Pizzeria! From store number {self.location_number }, What would you like to order? Pizza, Pasta, or a Salad? ")
+            if order_type == "Pizza":
+                order_price = 25.00
+                break
+            elif order_type == "Pasta":
+                order_price = 10.00
+                break
+            elif order_type == "Salad":
+                order_price = 5.00
+                break
+            else:
+                print("INVALID ENTRY. Please enter a valid order type (Pizza, Pasta, or Salad).")
 
         order = OrderFactory()
         order.create_order(order_type, self.location_number)
         logger = Logger()
-        logger.log_transaction(order_type, order_price, self.location_number )
+        logger.log_transaction(order_type, order_price, self.location_number)
